5126f39c5425cf96d3bc0b858d35edf0a198ab17
alber2007 <alberto.madrid@udla.edu.ec>
2026-03-29 20:05:25 -0500

    feat: ejercicio 1 completo

--- ejercicio1.c
+++ ejercicio1.c
@@ -2,28 +2,28 @@
 
 int main (int argc, char *argv[]) {
     
-//Desarrolle un programa en lenguaje C que solicite al usuario tres números enteros: Un número inicial, un número final, un valor de incremento
+//Desarrolle un programa en lenguaje C que solicite al usuario tres números enteros: Un número inicial, un número final y un valor de incremento
 //El programa debe mostrar en pantalla todos los números desde el número inicial hasta el número final, aplicando el incremento indicado, utilizando exclusivamente la estructura for.
     
     //Datos del ejercicio
-    int inicio, final, incremento;
+    int inicial, final, incremento;
 
     //Planteamiento del problema
     do
     {
     printf("Ingrese el numero inicial: ");
-    scanf("%d", &inicio);
-    if (inicio<0){
+    scanf("%d", &inicial);
+    if (inicial<0){
     printf("El valor inicial debe ser mayor que 0\n"); }
-    } while (inicio<0);
+    } while (inicial<0);
 
     do
     {
     printf("Ingrese el numero final: ");
     scanf("%d", &final);
-    if (final<=inicio){
+    if (final<=inicial){
     printf("El valor final debe ser mayor que el inicial\n"); }
-    } while (final<inicio);
+    } while (final<inicial);
 
     do
     {
@@ -34,7 +34,7 @@ int main (int argc, char *argv[]) {
     } while (incremento <=0); 
     
     //Ciclo for
-        for (int i=inicio; i<=final; i+=incremento) {
+        for (int i=inicial; i<=final; i+=incremento) {
             printf("%d ", i);    
         }
 
